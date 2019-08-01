339c18dd43585c6a3c379c149ad3a6a4b2e4872b
Ellen Omoto <ellen@localwise.com>
2019-08-01 01:14:05 -0700

    add urls and templates

--- app/views.py
+++ app/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
+from django.views import generic
 
-# Create your views here.
+from .models import Item
+
+class IndexView(generic.ListView):
+  template_name = 'app/index.html'
+  context_object_name = 'item_list'
+  def get_queryset(self):
+    return Item.objects.all()
+
+class DetailView(generic.DetailView):
+  model = Item
+  template_name = 'app/detail.html'
+
+def update(request, item_id):
+    item = get_object_or_404(Item, pk=item_id)
+    if "checkbox" in request.POST:
+      item.complete = True
+    else:
+      item.complete = False
+    item.save()
+    return HttpResponseRedirect(reverse('app:detail', args=(item.id,)))
