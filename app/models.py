13ad443e8a1e0fc19fca491fc7eaea6fc4124fde
Ellen Omoto <ellen@localwise.com>
2019-08-01 01:13:36 -0700

    add model stuff

--- app/models.py
+++ app/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Item(models.Model):
+  item_text = models.CharField(max_length=200)
+  complete = models.BooleanField(default=False)
+
+  def __str__(self):
+    return self.item_text
